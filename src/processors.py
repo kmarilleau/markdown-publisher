1b6c95baf28825a5ea9276b291f2b8204d465692
Kevin Marilleau <kevin.marilleau@gmail.com>
2020-12-06 21:18:24 +0100

    add BasicPostPathToUrlContentProcessor

--- src/processors.py
+++ src/processors.py
@@ -2,9 +2,11 @@ from abc import ABC, abstractmethod
 from typing import Optional
 from urllib.parse import urljoin
 
+from bs4 import BeautifulSoup
 from path import Path
 
 from . import Post
+from .utils.misc import is_absolute_url
 
 
 class PostProcessingError(ValueError):
@@ -23,7 +25,7 @@ class IProcessor(ABC):
 
 class ICanonicalURLProcessor(IProcessor):
     @abstractmethod
-    def get_absolute_url(self, relpath: Path, _from: Optional[Path] = None) -> str:
+    def get_absolute_url(self, path: Path, _from: Optional[Path] = None) -> str:
         ...
 
     def process(self, post: Post) -> Post:
@@ -31,20 +33,58 @@ class ICanonicalURLProcessor(IProcessor):
             raise PostProcessingError(f"{post.filepath} is not in {self.postsdir}.")
 
         return Post(
-            canonical_url=self.get_absolute_url(relpath=post.filepath),
+            canonical_url=self.get_absolute_url(path=post.filepath),
             **post.dict(exclude={"canonical_url"}),
         )
 
 
-class BasicPostGetAbsURLMixin(IProcessor):
-    def get_absolute_url(self, relpath: Path, _from: Optional[Path] = None) -> str:
-        if _from:
-            abspath = _from.parent.joinpath(relpath).abspath()
-        else:
-            abspath = relpath.abspath()
+class IPathToUrlContentProcessor(IProcessor):
+    @abstractmethod
+    def get_absolute_url(self, path: Path, _from: Optional[Path] = None) -> str:
+        ...
 
-        return urljoin(self.baseurl, self.postsdir.relpathto(abspath))
+    def process(self, post: Post) -> Post:
+        html_tree = BeautifulSoup(post.content, "lxml")
+
+        for tag in html_tree.find_all(href=True):
+            if not is_absolute_url(tag["href"]) and not any(
+                tag["href"].startswith(pattern) for pattern in ("tel:", "mailto:", "#")
+            ):
+                tag["href"] = self.get_absolute_url(
+                    path=Path(tag["href"]), _from=post.filepath
+                )
+
+        for tag in html_tree.find_all(src=True):
+            if not is_absolute_url(tag["src"]):
+                tag["src"] = self.get_absolute_url(
+                    path=Path(tag["src"]), _from=post.filepath
+                )
+
+        return Post(
+            content=html_tree.body.prettify(), **post.dict(exclude={"content"}),
+        )
+
+
+class BasicPostGetAbsURLMixin(IProcessor):
+    def get_absolute_url(self, path: Path, _from: Optional[Path] = None) -> str:
+        if path.isabs():
+            abspath = path
+        elif _from:
+            if path == ".":
+                abspath = _from
+            else:
+                abspath = _from.parent.joinpath(path).abspath()
+        else:
+            abspath = path.abspath()
+
+        return urljoin(self.baseurl, self.postsdir.relpathto(abspath)).rstrip("/")
 
 
 class BasicPostCanonicalURLProcessor(BasicPostGetAbsURLMixin, ICanonicalURLProcessor):
+    pass
+
+
+class BasicPostPathToUrlContentProcessor(
+    BasicPostGetAbsURLMixin, IPathToUrlContentProcessor
+):
     pass
--- tests/test_processors.py
+++ tests/test_processors.py
@@ -1,8 +1,13 @@
 import pytest
+from bs4 import BeautifulSoup
 from path import Path
 
 from src import Post
-from src.processors import BasicPostCanonicalURLProcessor, PostProcessingError
+from src.processors import (
+    BasicPostCanonicalURLProcessor,
+    BasicPostPathToUrlContentProcessor,
+    PostProcessingError,
+)
 
 URL = "https://test.fr"
 
@@ -58,3 +63,198 @@ class TestBasicPostCanonicalURLProcessor:
             processed_post.canonical_url
             == f"{URL}/{postsdir.relpathto(processed_post.filepath)}"
         )
+
+
+class TestBasicPostPathToUrlContentProcessor:
+    def test_abspath(self, post):
+        postsdir = Path("/postsdir")
+        filepath = postsdir / "post.md"
+        content = BeautifulSoup('<a href="{path}">Test</a>', "lxml").body.prettify()
+        path = "/absolute/path/image.png"
+        post = Post(
+            filepath=filepath,
+            content=content.format(path=path),
+            **post.dict(exclude={"filepath", "content"}),
+        )
+
+        processor = BasicPostPathToUrlContentProcessor(baseurl=URL, postsdir=postsdir)
+        processed_post = processor.process(post)
+
+        assert processed_post.content == content.format(path=URL + path)
+
+    def test_no_trailing_slash(self, post):
+        postsdir = Path("/postsdir")
+        filepath = postsdir / "post.md"
+        content = BeautifulSoup('<a href="{path}">Test</a>', "lxml").body.prettify()
+        path = "/absolute/path/trailing_slash/"
+        post = Post(
+            filepath=filepath,
+            content=content.format(path=path),
+            **post.dict(exclude={"filepath", "content"}),
+        )
+
+        processor = BasicPostPathToUrlContentProcessor(baseurl=URL, postsdir=postsdir)
+        processed_post = processor.process(post)
+
+        assert processed_post.content == content.format(path=URL + path.rstrip("/"))
+
+    def test_relpath_to_itself(self, post):
+        postsdir = Path("/postsdir")
+        filepath = postsdir / "post.md"
+        content = BeautifulSoup('<a href="{path}">Test</a>', "lxml").body.prettify()
+        post = Post(
+            filepath=filepath,
+            content=content.format(path="."),
+            **post.dict(exclude={"filepath", "content"}),
+        )
+
+        processor = BasicPostPathToUrlContentProcessor(baseurl=URL, postsdir=postsdir)
+        processed_post = processor.process(post)
+
+        assert processed_post.content == content.format(path=f"{URL}/{filepath.name}")
+
+    def test_href_tag(self, post):
+        postsdir = Path("/postsdir")
+        filepath = postsdir / "post.md"
+        content = BeautifulSoup('<a href="{path}">Test</a>', "lxml").body.prettify()
+        path = "test.html"
+        post = Post(
+            filepath=filepath,
+            content=content.format(path=path),
+            **post.dict(exclude={"filepath", "content"}),
+        )
+
+        processor = BasicPostPathToUrlContentProcessor(baseurl=URL, postsdir=postsdir)
+        processed_post = processor.process(post)
+
+        assert processed_post.content == content.format(path=f"{URL}/{path}")
+
+    def test_src_tag(self, post):
+        postsdir = Path("/postsdir")
+        filepath = postsdir / "post.md"
+        content = BeautifulSoup('<audio src="{path}">', "lxml").body.prettify()
+        path = "test.mp3"
+        post = Post(
+            filepath=filepath,
+            content=content.format(path=path),
+            **post.dict(exclude={"filepath", "content"}),
+        )
+
+        processor = BasicPostPathToUrlContentProcessor(baseurl=URL, postsdir=postsdir)
+        processed_post = processor.process(post)
+
+        assert processed_post.content == content.format(path=f"{URL}/{path}")
+
+    def test_relpath_to_baseurl(self, post):
+        postsdir = Path("/postsdir")
+        filepath = postsdir / "post.md"
+        content = BeautifulSoup('<a href="{path}">Test</a>', "lxml").body.prettify()
+        post = Post(
+            filepath=filepath,
+            content=content.format(path="/"),
+            **post.dict(exclude={"filepath", "content"}),
+        )
+
+        processor = BasicPostPathToUrlContentProcessor(baseurl=URL, postsdir=postsdir)
+        processed_post = processor.process(post)
+
+        assert processed_post.content == content.format(path=URL)
+
+    def test_no_relpath(self, post):
+        postsdir = Path("/postsdir")
+        filepath = postsdir / "post.md"
+        content = BeautifulSoup(
+            """
+<a href="https://google.com/test">Test</a>
+<div>
+    <img src="http://test.fr/path/image.jpeg">
+    <p>https://github.com/post<p>
+    <p>../workdir/subdir/posts.md<p>
+    Download src="test/dir/file.html"
+    <ul>
+      <li><a href="https://example.com">Website</a></li>
+      <li><a href="mailto:m.bluth@example.com">Email</a></li>
+      <li><a href="tel:+123456789">Phone</a></li>
+      <li><a href="#"></a></li>
+      <li><a href="#test">Anchor</a></li>
+    </ul>
+</div>
+<audio src="ftp://google.com/test">Test</a>
+<video src="https://google.com/test.mp4">./post.md</a>
+""",
+            "lxml",
+        ).body.prettify()
+        post = Post(
+            filepath=filepath,
+            content=content,
+            **post.dict(exclude={"filepath", "content"}),
+        )
+
+        processor = BasicPostPathToUrlContentProcessor(baseurl=URL, postsdir=postsdir)
+        processed_post = processor.process(post)
+
+        assert processed_post.content == content
+
+    def test_relpath_from_subdir_post(self, post):
+        postsdir = Path("/postsdir")
+        filepath = postsdir / "subdir1/post.md"
+        content = BeautifulSoup('<a href="{path}">Test</a>', "lxml").body.prettify()
+        path = "anotherfile.html"
+        post = Post(
+            filepath=filepath,
+            content=content.format(path=path),
+            **post.dict(exclude={"filepath", "content"}),
+        )
+
+        processor = BasicPostPathToUrlContentProcessor(baseurl=URL, postsdir=postsdir)
+        processed_post = processor.process(post)
+
+        assert processed_post.content == content.format(path=f"{URL}/subdir1/{path}")
+
+    def test_current_dir_relpath(self, post):
+        postsdir = Path("/postsdir")
+        filepath = postsdir / "post.md"
+        content = BeautifulSoup('<a href="{path}">Test</a>', "lxml").body.prettify()
+        path = "./anotherfile.html"
+        post = Post(
+            filepath=filepath,
+            content=content.format(path=path),
+            **post.dict(exclude={"filepath", "content"}),
+        )
+
+        processor = BasicPostPathToUrlContentProcessor(baseurl=URL, postsdir=postsdir)
+        processed_post = processor.process(post)
+
+        assert processed_post.content == content.format(path=f"{URL}/{path[2:]}")
+
+    def test_relpath_to_subdir_file(self, post):
+        postsdir = Path("/postsdir")
+        filepath = postsdir / "post.md"
+        content = BeautifulSoup('<a href="{path}">Test</a>', "lxml").body.prettify()
+        path = "subdir1/anotherfile.html"
+        post = Post(
+            filepath=filepath,
+            content=content.format(path=path),
+            **post.dict(exclude={"filepath", "content"}),
+        )
+
+        processor = BasicPostPathToUrlContentProcessor(baseurl=URL, postsdir=postsdir)
+        processed_post = processor.process(post)
+
+        assert processed_post.content == content.format(path=f"{URL}/{path}")
+
+    def test_relpath_to_file_in_parent_folder(self, post):
+        postsdir = Path("/postsdir")
+        filepath = postsdir / "subdir1/subdir2/post.md"
+        content = BeautifulSoup('<a href="{path}">Test</a>', "lxml").body.prettify()
+        path = "../test.html"
+        post = Post(
+            filepath=filepath,
+            content=content.format(path=path),
+            **post.dict(exclude={"filepath", "content"}),
+        )
+
+        processor = BasicPostPathToUrlContentProcessor(baseurl=URL, postsdir=postsdir)
+        processed_post = processor.process(post)
+
+        assert processed_post.content == content.format(path=f"{URL}/subdir1/test.html")
