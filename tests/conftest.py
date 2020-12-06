384e5dd7ba6744df2606a9af503f2d455529d5b4
Kevin Marilleau <kevin.marilleau@gmail.com>
2020-12-06 13:41:42 +0100

    mv post fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,21 @@
+import pytest
 import requests_cache
+from path import Path
+
+from src import Post, PostPublisher
 
 requests_cache.install_cache()
+
+
+@pytest.fixture
+def expected_post(scope="function"):
+    return Post(
+        filepath=Path("/workdir/postsdir/post.md"),
+        post_publisher=PostPublisher(),
+        title="My First Post",
+        canonical_url=None,
+        content="<h1>Content</h1><p>paragraph</p>",
+        tags=frozenset(["tag1", "tag2"]),
+        categories=frozenset(["cat1", "cat2"]),
+        is_draft=True,
+    )
--- tests/test_post_codecs.py
+++ tests/test_post_codecs.py
@@ -4,22 +4,8 @@ import frontmatter
 import pytest
 from path import Path
 
-from src import Post, PostPublisher
+from src import Post
 from src.post_codecs import HugoPostCodec, PostCodec, PostDecodeError
-
-
-@pytest.fixture
-def expected_post(scope="function"):
-    return Post(
-        filepath=Path(),
-        post_publisher=PostPublisher(),
-        title="My First Post",
-        canonical_url=None,
-        content="<h1>Content</h1><p>paragraph</p>",
-        tags=frozenset(["tag1", "tag2"]),
-        categories=frozenset(["cat1", "cat2"]),
-        is_draft=True,
-    )
 
 
 class TestPostCodec:
